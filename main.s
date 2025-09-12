26172ddf0b19c870f10757fbbc3c41a7df88bfb5
kobakobashu <kobaya4@fuji.waseda.jp>
2025-09-12 22:04:24 +0900

    impl binary add operation

--- main.s
+++ main.s
@@ -1,9 +1,20 @@
 .text
 .global main.main
 main.main:
-# *ast.BasicLit
-  movq $42, %rax
+# start *ast.BinaryExpr
+# start *ast.BasicLit
+  movq $40, %rax
   pushq %rax
+# end *ast.BasicLit
+# start *ast.BasicLit
+  movq $2, %rax
+  pushq %rax
+# end *ast.BasicLit
+  popq %rax
+  popq %rdi
+  addq %rdi, %rax
+  pushq %rax
+# end *ast.BinaryExpr
   popq %rax
   pushq %rax
   callq _os_exit
