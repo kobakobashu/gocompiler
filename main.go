e0078120623255fd3702a315e8ffcf1b77521e6e
kobakobashu <kobaya4@fuji.waseda.jp>
2025-09-20 15:40:35 +0900

    refactoring

--- main.go
+++ main.go
@@ -11,18 +11,18 @@ import (
 func emitExpr(expr ast.Expr) {
 	switch e := expr.(type) {
 	case *ast.ParenExpr:
-		fmt.Printf("# start %T\n", e)
+		fmt.Printf("  # start %T\n", e)
 		emitExpr(e.X)
-		fmt.Printf("# end %T\n", e)
+		fmt.Printf("  # end %T\n", e)
 	case *ast.BasicLit:
-		fmt.Printf("# start %T\n", e)
+		fmt.Printf("  # start %T\n", e)
 		val := e.Value
 		ival, _ := strconv.Atoi(val)
 		fmt.Printf("  movq $%d, %%rax\n", ival)
 		fmt.Printf("  pushq %%rax\n")
-		fmt.Printf("# end %T\n", e)
+		fmt.Printf("  # end %T\n", e)
 	case *ast.BinaryExpr:
-		fmt.Printf("# start %T\n", e)
+		fmt.Printf("  # start %T\n", e)
 		emitExpr(e.X) // left
 		emitExpr(e.Y) // right
 		if e.Op.String() == "+" {
@@ -43,13 +43,14 @@ func emitExpr(expr ast.Expr) {
 		} else {
 			panic(fmt.Sprintf("Unexpected binary operator %s", e.Op))
 		}
-		fmt.Printf("# end %T\n", e)
+		fmt.Printf("  # end %T\n", e)
 	case *ast.CallExpr:
-		fmt.Printf("# start %T\n", e)
+		fmt.Printf("  # start %T\n", e)
 		if len(e.Args) != 1 {
 			panic("Only single-argument calls are supported")
 		}
 		// Evaluate the single argument; leaves result on stack (top)
+		fmt.Printf("  # e.Args[0] type: %T\n", e.Args[0])
 		emitExpr(e.Args[0])
 		// Resolve callee symbol
 		switch fn := e.Fun.(type) {
@@ -65,7 +66,7 @@ func emitExpr(expr ast.Expr) {
 		default:
 			panic(fmt.Sprintf("Unsupported callee type %T", e.Fun))
 		}
-		fmt.Printf("# end %T\n", e)
+		fmt.Printf("  # end %T\n", e)
 	default:
 		panic(fmt.Sprintf("Unexpected expr type %T", expr))
 	}
--- main.s
+++ main.s
@@ -2,33 +2,34 @@
 .text
 .global main.main
 main.main:
-# start *ast.CallExpr
-# start *ast.BinaryExpr
-# start *ast.ParenExpr
-# start *ast.BinaryExpr
-# start *ast.BasicLit
+  # start *ast.CallExpr
+  # e.Args[0] type: *ast.BinaryExpr
+  # start *ast.BinaryExpr
+  # start *ast.ParenExpr
+  # start *ast.BinaryExpr
+  # start *ast.BasicLit
   movq $20, %rax
   pushq %rax
-# end *ast.BasicLit
-# start *ast.BasicLit
+  # end *ast.BasicLit
+  # start *ast.BasicLit
   movq $1, %rax
   pushq %rax
-# end *ast.BasicLit
+  # end *ast.BasicLit
   popq %rdi # right
   popq %rax # left
   addq %rdi, %rax
   pushq %rax
-# end *ast.BinaryExpr
-# end *ast.ParenExpr
-# start *ast.BasicLit
+  # end *ast.BinaryExpr
+  # end *ast.ParenExpr
+  # start *ast.BasicLit
   movq $2, %rax
   pushq %rax
-# end *ast.BasicLit
+  # end *ast.BasicLit
   popq %rdi # right
   popq %rax # left
   imulq %rdi, %rax
   pushq %rax
-# end *ast.BinaryExpr
+  # end *ast.BinaryExpr
   callq os.Exit
-# end *ast.CallExpr
+  # end *ast.CallExpr
   ret
