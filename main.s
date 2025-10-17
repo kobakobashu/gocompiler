1a59e985c388de2eb6aaf3cd014fdf252d1e2857
kobakobashu <kobaya4@fuji.waseda.jp>
2025-10-17 23:43:58 +0900

    handle global variable declarations and expressions of them

--- main.s
+++ main.s
@@ -1,9 +1,26 @@
+# Package  "./t"
+# Name:    main
+# Unresolved: []
+# Package:   main
+# valSpec.type=&ast.Ident{NamePos:37, Name:"int", Obj:(*ast.Object)(0x75d160)}
+# valSpec.type=&ast.Ident{NamePos:59, Name:"string", Obj:(*ast.Object)(0x75d100)}
+# spec.Name=myString, Value=&{68 STRING "to stderr\n"}
 .data
+# string literals
 .S0:
-  .string "hello world\n"
-.S1:
   .string "to stderr\n"
-
+# string literals
+.S1:
+  .string "hello world\n"
+# string literals
+.S2:
+  .string "to stderr\n"
+# Global Variables
+myInt:  #
+  .quad 40
+myString:  #
+  .quad .S0
+  .quad 10
 # funcDecl main
 .text
 main.main:
@@ -12,7 +29,7 @@ movq %rsp, %rbp
   # funcall=*ast.Ident
   # start *ast.BasicLit
   # kind=STRING
-  leaq .S0, %rax
+  leaq .S1, %rax
   pushq %rax
   pushq $12
   # end *ast.BasicLit
@@ -20,29 +37,27 @@ movq %rsp, %rbp
   # funcall=*ast.Ident
   # start *ast.BasicLit
   # kind=STRING
-  leaq .S1, %rax
+  leaq .S2, %rax
   pushq %rax
   pushq $10
   # end *ast.BasicLit
   callq runtime.printstring
+  # funcall=*ast.Ident
+  # ident kind=var
+  # Obj=&ast.Object{Kind:4, Name:"myString", Decl:(*ast.ValueSpec)(0xc0000593b0), Data:0, Type:interface {}(nil)}
+  # emitVariable string ident.Decl.Type=&ast.Ident{NamePos:59, Name:"string", Obj:(*ast.Object)(0x75d100)}
+  movq myString+0(%rip), %rax
+  pushq %rax # ptr
+  mov myString+8(%rip), %rax
+  pushq %rax # len
+  callq runtime.printstring
   # funcall=*ast.SelectorExpr
   # start *ast.BinaryExpr
-  # start *ast.BinaryExpr
-  # start *ast.BasicLit
-  # kind=INT
-  movq $20, %rax
-  pushq %rax
-  # end *ast.BasicLit
-  # start *ast.BasicLit
-  # kind=INT
-  movq $1, %rax
-  pushq %rax
-  # end *ast.BasicLit
-  popq %rdi # right
-  popq %rax # left
-  addq %rdi, %rax
-  pushq %rax
-  # end *ast.BinaryExpr
+  # ident kind=var
+  # Obj=&ast.Object{Kind:4, Name:"myInt", Decl:(*ast.ValueSpec)(0xc000059360), Data:0, Type:interface {}(nil)}
+  # emitVariable string ident.Decl.Type=&ast.Ident{NamePos:37, Name:"int", Obj:(*ast.Object)(0x75d160)}
+  movq myInt+0(%rip), %rax
+  pushq %rax # ptr
   # start *ast.BasicLit
   # kind=INT
   movq $2, %rax
@@ -50,7 +65,7 @@ movq %rsp, %rbp
   # end *ast.BasicLit
   popq %rdi # right
   popq %rax # left
-  imulq %rdi, %rax
+  addq %rdi, %rax
   pushq %rax
   # end *ast.BinaryExpr
   callq os.Exit
