f70a4a36db441fde9a7dde7e93c59f3e4a02044d
kobakobashu <kobaya4@fuji.waseda.jp>
2025-10-24 23:04:26 +0900

    refactoring

--- t/source.go
+++ t/source.go
@@ -3,11 +3,12 @@ package main
 import "os"
 
 var myInt int = 40
+var myInt2 int = 2
 var myString string = "to stderr\n"
 
 func main() {
 	print("hello world\n")
 	print("to stderr\n")
 	print(myString)
-	os.Exit(myInt + 2)
+	os.Exit(myInt + myInt2)
 }
